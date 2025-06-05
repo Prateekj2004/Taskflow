0b2aba9d119c3d65fcb2e784e8f6ab425e6b505f
Prateek Jain <prateekjdelhi2004@gmail.com>
2025-06-06 03:39:47 +0530

    sign up page created

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=> {
-    await mongoose.connect('mongodb+srv://prateekjdelhi2004:r4dmwxRWFj9aUsSX@cluster0.cskfcn6.mongodb.net/taskflow')
-        .then(()=>console.log('DB CONNECTED'));
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://prateekjdelhi2004:r4dmwxRWFj9aUsSX@cluster0.cskfcn6.mongodb.net/taskflow?retryWrites=true&w=majority'
+    );
+    console.log('DB CONNECTED');
+  } catch (error) {
+    console.error('DB connection error:', error);
+  }
+};
